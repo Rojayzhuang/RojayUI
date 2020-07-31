2d93308e59d2f9cc43a75ab43ff2a1a8d0b201f8
庄鲁杰 <sx_099129@travelsky.com>
2020-07-31 18:43:12 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Christe&Rojay</div>
+  <!直接引用Frank>
+  <Frank/>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+    //或者缩写为一个Frank
+  }
+}
+</script>
