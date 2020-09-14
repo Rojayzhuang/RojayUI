584bd87ddb00f390d0dab2e13fa76ef006bbbda1
rojayzhuang <651577372@qq.com>
2020-09-14 17:14:41 +0800

    初始化aside的html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
-        <Topnav/>
-        <div class="content">
-            <aside>边栏</aside>
-            <main>主内容</main>
-        </div>
+<div>
+    <Topnav />
+    <div class="content">
+        <aside>
+            <h2>组件列表</h2>
+            <ol>
+                <li>
+                    <router-link to="/doc/swich">Swich 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/button">Button 组件</router-link>
+                </li>
+                <li>
+                    <router-linke to="/doc/dialog">Dialog 组件</router-linke>
+                </li>
+                <li>
+                    <router-linke to="/doc/tabs">Tabs 组件</router-linke>
+                </li>
+            </ol>
+        </aside>
+        <main>主内容</main>
     </div>
+</div>
 </template>
+
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
 export default {
-  components: {Topnav} 
+    components: {
+        Topnav
+    }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,48 @@
 <template>
-    <div>
+<div>
     <Topnav />
-        <div class="banner">
-            <h1>RojayUI</h1>
-            <h2>Rojay自造的 UI 框架</h2>
-            <p class="actions">
-                <a href="https://github.com/Rojayzhuang">GitHub</a>
-                <router-link to="/doc">开始</router-link>
-            </p>
-        </div>
+    <div class="banner">
+        <h1>RojayUI</h1>
+        <h2>Rojay自造的 UI 框架</h2>
+        <p class="actions">
+            <a href="https://github.com/Rojayzhuang">GitHub</a>
+            <router-link to="/doc">开始</router-link>
+        </p>
     </div>
+</div>
 </template>
-<script lang = "ts">
+
+<script>
 import Topnav from "../components/Topnav.vue";
 export default {
-  components: { Topnav }
+    components: {
+        Topnav
+    }
 };
 </script>
+
 <style lang="scss" scoped>
 .banner {
-  padding: 100px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background: lightgreen;
-  > .actions{
-    padding: 8px 0;
-    a {
-       margin:0 8px;
-      background: #fff;
-      display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+
+    >.actions {
+        padding: 8px 0;
+
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
     }
-  }
 }
 </style>
