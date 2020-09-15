78117696abe2016a0ce8e77a8c3e4227d67171af
rojayzhuang <651577372@qq.com>
2020-09-15 11:00:11 +0800

    调节Topnav和aside的位置

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="topnav">
+<div class="topnav">
     <div class="logo">LOGO</div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+        <li>菜单1</li>
+        <li>菜单2</li>
     </ul>
-  </div>
+</div>
 </template>
+
 <script lang="ts">
 export default {
-  
+
 }
 </script>
+
 <style lang="scss" scoped>
 .topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
+    background: pink;
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+    padding: 16px;
+    position: relative;
+    z-index: 10;
+
+    >.logo {
+        max-width: 6em;
+        margin-right: auto;
     }
-  }
+
+    >.menu {
+        display: flex;
+        white-space: nowrap;
+        flex-wrap: nowrap;
+
+        >li {
+            margin: 0 1em;
+        }
+    }
 }
-</style> 
+</style>
