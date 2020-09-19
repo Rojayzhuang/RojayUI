9af4d60fcfc6b1a931e022a6b265b9e754cda8ea
rojayzhuang <651577372@qq.com>
2020-09-19 22:11:47 +0800

    实现切换效果

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,25 @@
 <template>
-<button> <span></span> </button>
+<button @click="toggle" :class="{checked}"> <span></span> </button>
 </template>
+
+<script lang="ts">
+import {
+    ref
+} from 'vue'
+export default {
+    setup() {
+        //默认false
+        const checked = ref(false);
+        const toggle = () => {
+            checked.value = !checked.value
+        }
+        return {
+            checked,
+            toggle
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 $h:22px;
@@ -10,7 +29,8 @@ button {
     height: $h;
     width: $h*2;
     border: none;
-    background: blue;
+    //默认状态为灰色
+    background: gray;
     border-radius: $h/2;
     position: relative;
 }
@@ -26,8 +46,13 @@ span {
     border-radius: $h2 / 2;
 }
 
+//如果button的状态是checked（被触发）则背景变为蓝色
+button.checked {
+    background: blue;
+}
+
 //按钮切换的动画
-button:hover>span {
+button.checked>span {
     //使用CSS的高级样式
     left: calc(100% - #{$h2} - 2px);
 }
