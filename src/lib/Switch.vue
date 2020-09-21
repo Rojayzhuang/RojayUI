d1e039d6435b94a14564d75c7c1c3a21b47821f3
rojayzhuang <651577372@qq.com>
2020-09-21 17:50:19 +0800

    优化CSS代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -35,46 +35,46 @@ button {
     background: #bfbfbf;
     border-radius: $h/2;
     position: relative;
-}
 
-span {
-    position: absolute;
-    top: 2px;
-    //默认2像素，触发事件后切换
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background: white;
-    border-radius: $h2 / 2;
-    transition: all 250ms;
-}
-
-//如果button的状态是checked（被触发）则背景变为蓝色
-button.checked {
-    background: #1890ff;
-}
-
-//按钮切换的动画
-button.checked>span {
-    //使用CSS的高级样式
-    left: calc(100% - #{$h2} - 2px);
-}
-
-//解决点击switch控件出现黑框的问题
-button:focus {
-    outline: none;
-}
-
-button:active {
     >span {
-        width: $h2 + 4px;
+        position: absolute;
+        top: 2px;
+        //默认2像素，触发事件后切换
+        left: 2px;
+        height: $h2;
+        width: $h2;
+        background: white;
+        border-radius: $h2 / 2;
+        transition: all 250ms;
     }
-}
 
-button.checked:active {
-    >span {
-        width: $h2 + 4px;
-        margin-left: -4px;
+    //如果button的状态是checked（被触发）则背景变为蓝色
+    &.checked {
+        background: #1890ff;
+
+        //按钮切换的动画
+        >span {
+            //使用CSS的高级样式
+            left: calc(100% - #{$h2} - 2px);
+        }
+    }
+
+    //解决点击switch控件出现黑框的问题
+    &:focus {
+        outline: none;
+    }
+
+    &:active {
+        >span {
+            width: $h2 + 4px;
+        }
+    }
+
+    &.checked:active {
+        >span {
+            width: $h2 + 4px;
+            margin-left: -4px;
+        }
     }
 }
 </style>
