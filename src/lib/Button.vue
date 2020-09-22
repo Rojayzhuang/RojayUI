8e4c2c330c2e245aa1468f8d466f7231b8855048
rojayzhuang <651577372@qq.com>
2020-09-22 18:31:46 +0800

    创建Button组件

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,35 @@
 <template>
-<button>
-    <slot />
-</button>
+<div :size="size">
+    <button v-bind="rest">
+        <slot />
+    </button>
+</div>
 </template>
+
+<script lang="ts">
+export default {
+    //继承属性改为false，这样div不会默认继承
+    inheritAttrs: false,
+    props: {
+
+    },
+    setup(props, context) {
+
+        //多个属性，使用const{size, level, ...xxx}=context.attrs将属性分开
+        const {
+            size,
+            ...rest
+        } = context.attrs
+        return {
+            size,
+            rest
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+    border: 1px solid red;
+}
+</style>
