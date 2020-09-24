4b306964d0bbe8485b92c96352f825c3fc277ce6
rojayzhuang <651577372@qq.com>
2020-09-24 15:07:50 +0800

    size css 2

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -107,18 +107,17 @@ $radius: 4px;
         }
     }
 
-    &.rojay-theme-button {
-        &.rojay-size-big {
-            font-size: 24px;
-            height: 48px;
-            padding: 0 16px;
-        }
-
-        &.rojay-size-small {
-            font-size: 12px;
-            height: 20px;
-            padding: 0 4px;
-        }
+    &.rojay-size-big {
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px;
     }
+
+    &.rojay-size-small {
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
+    }
+
 }
 </style>
