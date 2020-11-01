188defe21901d64b2a2a868c6b6121947aaf0509
rojayzhuang <651577372@qq.com>
2020-11-01 21:02:43 +0800

    极致优化markdown

--- src/router.ts
+++ src/router.ts
@@ -6,10 +6,12 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
-import Intro from "./views/Intro.vue";
-import GetStarted from "./views/GetStarted.vue";
-import Install from "./views/Install.vue";
-import Note from "./views/Note.vue";
+
+import Markdown from './components/Markdown.vue';
+import { h } from "vue";
+
+const md = filename =>  h(Markdown, {path: `../markdown/${filename}.md`, key:filename})
+
 
 //新建history对象
 const history = createWebHashHistory();
@@ -23,10 +25,10 @@ export const router = createRouter({
         {path: "/doc", component: Doc, 
         children:[
             {path: "", component: DocDemo },
-            {path: "intro", component: Intro },
-            {path: "get-started", component: GetStarted },
-            {path: "install", component: Install },
-            {path: "note", component: Note },
+            {path: "intro", component: md('intro')},
+            {path: "get-started", component: md('get-started') },
+            {path: "install", component: md('install') },
+            {path: "note", component: md('note') },
             {path: "switch", component: SwitchDemo },
             {path: "button", component: ButtonDemo },
             {path: "dialog", component: DialogDemo },
