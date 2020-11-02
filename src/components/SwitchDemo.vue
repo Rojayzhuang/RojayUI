35a8aed797329d564e2be2c844bbc9adff9874ee
rojayzhuang <651577372@qq.com>
2020-11-02 22:36:11 +0800

    更新 SwitchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,20 +1,48 @@
 <template>
+<!--需要监听Switch按钮的值，插入input事件，监听 -->
+<!-- Switch :value="y" @update:value="y = $event" / -->
+<!-- 将上述代码简化，使用v-model，将对update监听自动加上-->
+<!--<Switch v-model:value="bool" /> -->
+
 <div>
-    <!--需要监听Switch按钮的值，插入input事件，监听 -->
-    <!-- Switch :value="y" @update:value="y = $event" / -->
-    <!-- 将上述代码简化，使用v-model，将对update监听自动加上-->
-    <Switch v-model:value="bool" />
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+        <h2>常规用法</h2>
+        <div class="demo-component">
+            <Switch v-model:value="bool" />
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        </div>
+    </div>
+    <div class="demo">
+        <h2>支持 disabled </h2>
+        <div class="demo-component">
+            <Switch v-model:value="bool" disabled />
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        </div>
+    </div>
 </div>
 </template>
 
 <script lang="ts">
+import Switch from '../lib/Switch.vue'
+import Button from '../lib/Button.vue'
 import {
     ref
 } from 'vue'
-import Switch from '../lib/Switch.vue'
 export default {
     components: {
-        Switch
+        Switch,
+        Button
     },
     setup() {
         const bool = ref(false)
@@ -24,3 +52,38 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+
+.demo {
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
+
+    >h2 {
+        font-size: 20px;
+        padding: 8px 16px;
+        border-bottom: 1px solid $border-color;
+    }
+
+    &-component {
+        padding: 16px;
+    }
+
+    &-actions {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+    }
+
+    &-code {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+
+        >pre {
+            line-height: 1.1;
+            font-family: Consolas, 'Courier New', Courier, monospace;
+            margin: 0;
+        }
+    }
+}
+</style>
