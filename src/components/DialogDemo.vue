26e9de16e73ea8eacb35c397d2fb14bdb795422e
rojayzhuang <651577372@qq.com>
2020-11-21 23:42:22 +0800

    改造 Tabs Demo

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,69 +1,23 @@
 <template>
-<div>
-    DialogDemo 示例
-</div>
-
-<h1>示例1</h1>
-<Button @click="toggle">toggle</Button>
-<!--<Dialog :visible="x" @update:visible="x = $event"> </Dialog>-->
-<!--可以优化为,点击ok时执行f1函数，点击cancel时执行f2函数-->
-<Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
-    <template v-slot:content>
-        <strong>hi</strong>
-        <div>hi2</div>
-    </template>
-    <template v-slot:title>
-        <strong>加粗的标题</strong>
-    </template>
-</Dialog>
-
-<!-- 希望能更简单的打开Dialog -->
-<h1>示例2</h1>
-<Button @click="showDialog">show</Button>
+  <h1>DialogDemo 示例</h1>
+  <Demo :component="Dialog1Demo" />
+  <Demo :component="Dialog2Demo" />
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
-import Button from '../lib/Button.vue'
-import {
-    ref
-} from 'vue'
-import {
-    openDialog
-} from '../lib/openDialog'
+import Demo from "./Demo.vue";
+import Dialog1Demo from "./Dialog1.demo.vue";
+import Dialog2Demo from "./Dialog2.demo.vue";
+
 export default {
-    components: {
-        Dialog,
-        Button
-    },
-    setup() {
-        const x = ref(false)
-        const toggle = () => {
-            x.value = !x.value
-        }
-        const f1 = () => {
-            return false
-        }
-        const f2 = () => {}
-        const showDialog = () => {
-            openDialog({
-                title: '标题',
-                content: '你好',
-                ok() {
-                    console.log('ok')
-                },
-                cancel() {
-                    console.log('cancel')
-                }
-            })
-        }
-        return {
-            x,
-            toggle,
-            f1,
-            f2,
-            showDialog
-        }
-    }
-}
+  components: {
+    Demo,
+  },
+  setup() {
+    return {
+      Dialog1Demo,
+      Dialog2Demo,
+    };
+  },
+};
 </script>
